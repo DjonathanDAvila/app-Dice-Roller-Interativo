94c6e98a42d39bf1bead5705f7002f87729a52bf
Djonathan <djonathandavila.dev@gmail.com>
2023-01-31 09:39:23 -0300

    Adicionado a lógica para jogar os dados

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,17 +6,36 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result on the screen
+ * Esta atividade permite ao usuário jogar um dado e vesualizar o resultado na tela
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
-            // val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show() 1.
-            //toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-        }
+        rollButton.setOnClickListener { rollDice() }
+    }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     * Role os dados e atualize a tela com o resultado
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        var diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
